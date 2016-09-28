530a174117a07c626445e7fa69a1f18ef47a1872
Amit singh <amitsin6h@gmail.com>
2016-09-28 16:58:29 +0530

    created c.php and modified index2.php

--- index2.php
+++ index2.php
@@ -1,1 +1,1 @@
-<!DOCTYPE html><html lang='eng'><head><title>Graphs and Charts</title></head><body><p>Graphs and Chartst</p><p><a href="a.php">Graph A</a></p><p><a href="b.php">Graph B</a></p></body></html>
+<!DOCTYPE html><html lang='eng'><head><title>Graphs and Charts</title></head><body><p>Graphs and Chartst</p><p><a href="a.php">Graph A</a></p><p><a href="b.php">Graph B</a></p><p><a href="c.php">Graph C</a></p></body></html>
